4fa861046c40b67d549726a2a696bb3451fe7a77
irek2101 <99901412+irek2101@users.noreply.github.com>
2025-10-26 19:25:58 +0100

    Add files via upload

--- node.lua
+++ node.lua
@@ -1,11 +1,14 @@
 
 gl.setup(1920, 1080)
 
+-- white texture
+local white = resource.create_colored_texture(1,1,1,1)
+local red   = resource.create_colored_texture(1,0,0,1)
+
 function node.render()
-  -- navy background
-  gl.clear(0.05, 0.08, 0.18, 1)
-  -- big white rectangle in the middle
-  gl.color(1,1,1,1)
-  local margin = 200
-  gl.rect(margin, margin, 1920 - margin, 1080 - margin)
+  gl.clear(0.05, 0.08, 0.18, 1) -- navy
+  -- big white box
+  white:draw(200, 200, 1720, 880, 1.0)
+  -- small red corner marker (should be clearly visible)
+  red:draw(40, 40, 140, 100, 1.0)
 end
